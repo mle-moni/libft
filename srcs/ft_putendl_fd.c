d4f9ba08991b8738488e94bce476a15b20acea81
Mayeul LE MONIES DE SAGAZAN <mle-moni@e3r11p16.42.fr>
2019-10-09 11:34:16 +0200

    cr into new line ù_ú

--- srcs/ft_putendl_fd.c
+++ srcs/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: mle-moni <mle-moni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 12:10:42 by mle-moni          #+#    #+#             */
-/*   Updated: 2019/10/08 12:15:04 by mle-moni         ###   ########.fr       */
+/*   Updated: 2019/10/09 11:33:30 by mle-moni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ void	ft_putendl_fd(char *s, int fd)
 	while (s[i])
 		i++;
 	write(fd, s, i);
-	write(fd, "\r", 1);
+	write(fd, "\n", 1);
 }
