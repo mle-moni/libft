57b2e7022849ebdc490bcccd4cdef99c18c012b0
Mayeul LE MONIES DE SAGAZAN <mle-moni@e3r11p16.42.fr>
2019-10-09 11:01:56 +0200

    memmove debug

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mle-moni <mle-moni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 12:10:11 by mle-moni          #+#    #+#             */
-/*   Updated: 2019/10/07 16:27:14 by mle-moni         ###   ########.fr       */
+/*   Updated: 2019/10/09 10:56:49 by mle-moni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	while (1)
 	{
 		((unsigned char*)dest)[n] = ((unsigned char*)src)[n];
-		n--;
 		if (n == 0)
 			return (dest);
+		n--;
 	}
 }
