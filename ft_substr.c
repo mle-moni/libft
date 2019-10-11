b5f571cfed1626588e49d3ef69fbf4b29c6696d6
Mayeul LE MONIES DE SAGAZAN <mle-moni@e1r7p16.42.fr>
2019-10-11 10:54:50 +0200

    substr bettered

--- ft_substr.c
+++ ft_substr.c
@@ -6,18 +6,34 @@
 /*   By: mle-moni <mle-moni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 16:17:31 by mle-moni          #+#    #+#             */
-/*   Updated: 2019/10/07 16:36:36 by mle-moni         ###   ########.fr       */
+/*   Updated: 2019/10/11 10:54:30 by mle-moni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+static unsigned int	length(const char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char				*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 	size_t	i;
 
 	i = 0;
+	if (start > length(s) || (start + len) > length(s))
+	{
+		str = malloc(1);
+		str[0] = '\0';
+		return (str);
+	}
 	str = malloc((len + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
