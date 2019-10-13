3d75d21b3f99ea7f6222b48ccfe34d54fd391349
Mayeul LE MONIES DE SAGAZAN <mle-moni@e2r9p4.42.fr>
2019-10-13 12:07:11 +0200

    malloc protection

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: mle-moni <mle-moni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 11:44:29 by mle-moni          #+#    #+#             */
-/*   Updated: 2019/10/08 12:06:02 by mle-moni         ###   ########.fr       */
+/*   Updated: 2019/10/13 12:05:46 by mle-moni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	while (s[i])
 		i++;
 	str_total = malloc((i + 1) * sizeof(char));
+	if (!str_total)
+		return (NULL);
 	i = 0;
 	while (s[i])
 	{
