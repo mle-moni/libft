7d8d7155c989240b32f109d2746d71cd0696dcee
Mayeul LE MONIES DE SAGAZAN <mle-moni@e1r11p12.42.fr>
2019-10-17 16:13:20 +0200

    segfault when null params !!!!!!!!!!!

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mle-moni <mle-moni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 14:04:25 by mle-moni          #+#    #+#             */
-/*   Updated: 2019/10/09 15:13:50 by mle-moni         ###   ########.fr       */
+/*   Updated: 2019/10/17 16:13:02 by mle-moni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 	int		diff;
 
+	if ((!s1 && !s2) || n == 0)
+		return (0);
 	diff = 0;
 	i = 0;
 	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
